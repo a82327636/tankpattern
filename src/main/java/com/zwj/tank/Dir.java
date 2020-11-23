c37a1017995dc34745a60ad5f9d0cae0fac7988f
zwj z <zwj@example.com>
2020-11-23 14:57:11 +0800

    THIRD 添加枚举方向

---
+++ src/main/java/com/zwj/tank/Dir.java
@@ -0,0 +1,15 @@
+package com.zwj.tank;
+
+/**
+ * @ProjectName: tankpattern
+ * @Package: com.zwj.tank
+ * @ClassName: Dir
+ * @Author: zwj
+ * @Description: 注释
+ * @Date: 2020/11/23 14:49
+ * @Version: 1.0
+ */
+// 方向
+public enum Dir {
+    LEFT,UP,RIGHT,DOWN
+}
