e05ddfb23b04e36f66cba716d20d3aa986d1edf4
zwj z <zwj@example.com>
2020-11-24 09:53:31 +0800

    THIRD

--- src/main/java/com/zwj/tank/ResourceMgr.java
+++ src/main/java/com/zwj/tank/ResourceMgr.java
@@ -17,12 +17,23 @@ public class ResourceMgr {
 
     public static BufferedImage tankL,tankU,tankR,tankD;
 
+    public static BufferedImage bulletL,bulletU,bulletR,bulletD;
+
+
+
     static{
         try {
             tankD = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("config/images/tank_down.png"));
             tankL = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("config/images/tank_left.png"));
             tankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("config/images/tank_up.png"));
             tankR = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("config/images/tank_right.png"));
+
+
+            bulletL = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("config/images/bullet.png"));
+            bulletU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("config/images/bullet.png"));
+            bulletR = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("config/images/bullet.png"));
+            bulletD = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("config/images/bullet.png"));
+
         } catch (IOException e) {
             e.printStackTrace();
         }
